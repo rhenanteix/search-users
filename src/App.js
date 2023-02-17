f56f0dc9c4a6ec2e57290af3e74c4f6d5a598fa5
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2023-02-17 14:05:35 -0300

    root pages index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Layout from './components/Layout';
+import Profile from './components/Profile';
+import { darkTheme, lightTheme } from './components/Theme';
+import { GlobalCss } from './global/Global';
+import GithubProvider from './providers/GithubProvider';
 
-function App() {
+const App = () => {
+  const [theme] = useState('light');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalCss />
+      <GithubProvider>
+        <Layout>
+          <Profile />
+        </Layout>
+      </GithubProvider>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
